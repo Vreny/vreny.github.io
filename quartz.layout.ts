93c367116be0aeedba512a062408846a76c12467
Kacper <83926667+Vreny@users.noreply.github.com>
2026-05-22 23:54:25 +0300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,53 +1,91 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
 
-export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [
-    Component.Search(),
-    Component.Darkmode(),
-  ],
-  afterBody: [],
-  footer: Component.Footer({
-    links: {},
-  }),
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Vreny|Kacper",
+    pageTitleSuffix: "",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "en-US",
+    baseUrl: "vreny.github.io",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "modified",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#6e40aa",
+          tertiary: "#a271eb",
+          highlight: "rgba(110, 64, 170, 0.15)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#212121",
+          lightgray: "#2d2d2d",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#a271eb",
+          tertiary: "#ca9bf7",
+          highlight: "rgba(162, 113, 235, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      Plugin.CustomOgImages(),
+    ],
+  },
 }
 
-export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Explorer({
-      title: "Contents",
-      folderClickBehavior: "toggle",
-      filterFn: (node) => node.name !== "tags",
-    }),
-  ],
-  right: [
-    Component.Graph(),
-    Component.TableOfContents(),
-    Component.Backlinks(),
-  ],
-}
-
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Explorer({ 
-      title: "Contents",
-      folderClickBehavior: "toggle",
-      filterFn: (node) => node.name !== "tags",
-    }),
-  ],
-  right: [
-    Component.Graph(),
-    Component.Backlinks(),
-  ],
-}
+export default config
